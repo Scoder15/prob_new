fe89430d20e240d27fae21ace2c49ecc648b9455
shenguoming <shenguoming@xiaohongshu.com>
2022-04-08 15:57:28 +0800

    001

---
+++ 001.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println(divide(2, 4))
+	tot := 1
+	tot += 1 << 2
+	fmt.Println(tot)
+
+}
+
+func divide(a, b int) int {
+	sign := 1
+	if a*b < 0 {
+		sign = -1
+
+	}
+	a = abs(a)
+	b = abs(b)
+	tot := 0
+	for a > b {
+		cnt := 0
+		for a >= b<<(cnt+1) {
+			cnt += 1
+		}
+		tot += 1 << cnt
+		a -= b << cnt
+	}
+
+	ans := sign * tot
+	if ans >= math.MinInt32 && ans <= math.MaxInt32 {
+		return ans
+
+	}
+	return math.MaxInt32
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func qpow(a, n int) int {
+	ans := 1
+	for a > 0 {
+		if n&1 > 0 {
+			ans *= a
+		}
+		a *= a
+		n >>= 1
+	}
+	return ans
+}
