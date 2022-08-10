e10529822c7a75f41ee221e61d37c3d8f6fcc245
shenguoming <shenguoming@xiaohongshu.com>
2022-08-10 21:17:05 +0800

    sh

---
+++ prob/034.go
@@ -0,0 +1,30 @@
+package prob
+
+func isAlienSorted(words []string, order string) bool {
+	index := make(map[byte]int)
+	for i := 0; i < len(words)-1; i++ {
+		w1, w2 := words[i], words[i+1]
+		l1, l2 := len(w1), len(w2)
+		flag := true
+		for j := 0; j < min(l1, l2); j++ {
+			i1, i2 := index[w1[j]], index[w2[j]]
+			if i1 > i2 {
+				return false
+			}
+			if i1 < i2 {
+				flag = false
+			}
+		}
+		if flag && l1 > l2 {
+			return false
+		}
+	}
+	return true
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ prob/036.go
@@ -0,0 +1,8 @@
+package prob
+
+import "https://github.com/emirpasic/gods#arraystack"
+
+func evalRPN(tokens []string) int {
+	stk := arraystack.New()
+	return 0
+}
