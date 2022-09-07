d3e410bc2d6cef2eedca2f786e99c11d78869a40
shenguoming <shenguoming@xiaohongshu.com>
2022-09-07 20:06:59 +0800

    MOD:38

--- prob/038.go
+++ prob/038.go
@@ -1,14 +1,27 @@
 package prob
 
 func dailyTemperatures(temperatures []int) []int {
-	return []int{}
+	res := make([]int, len(temperatures))
+	var stk []int
+	for i, t := range temperatures {
+		for len(stk) > 0 && temperatures[stk[len(stk)-1]] < t {
+			j := stk[len(stk)-1]
+			res[j] = i - j
+			stk = stk[:len(stk)-1]
+		}
+		stk = append(stk, i)
+	}
+	return res
 }
 
 func onlyStack(arr []int) {
 	stk := make([]int, 0)
-	for _, v := range arr {
+	for i, v := range arr {
 		//满足条件将栈顶元素出栈，将当前元素入栈。
 		stk = append(stk, v)
+		//pop
+		stk = stk[:len(stk)-1]
+		stk = append(stk, i)
 	}
 
 }
